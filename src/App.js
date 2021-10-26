86041ab360e451fec0fb7a26924eed2cc73a15f3
uplusion23 <uplusion23@gmail.com>
2021-10-26 15:05:29 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isAddRecipeFormDisplayed, setIsAddRecipeFormDisplayed] = useState(false);
+
+
+  const saveRecipe = (event) => {
+    event.preventDefault();
+    setRecipes([...recipes, {
+      name: event.target.elements.newRecipeName.value,
+      instructions: event.target.elements.newRecipeInstructions.value
+    }]);
+  }
+
+  const addNewRecipeForm = (
+    <form id="recipe-form" onSubmit={saveRecipe}>
+      <label htmlFor="newRecipeName">Recipe name: </label>
+      <input type="text" id="newRecipeName" />
+      <label htmlFor="newRecipeInstructions">Instructions:</label>
+      <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." />
+      <input type="submit" />
+    </form>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">My Recipes</h1>
+      {
+        isAddRecipeFormDisplayed
+          ? addNewRecipeForm
+          : <button id="add-recipe" onClick={() => setIsAddRecipeFormDisplayed(true)} > Add Recipe</button>
+      }
+      {
+        (recipes.length > 0)
+        ? <ul>
+            {
+              recipes.map((recipe, index) => {
+                return <li key={index}>{recipe.name}</li>
+              }
+            )}
+          </ul>
+        : <p>There are no recipes to list.</p>
+      }
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Add recipe button toggles visibility of a form on the page ', () => {
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // `queryBy...` methods will return null if the element is not found:
+  const recipeForm = screen.queryByText("Instructions:");
+
+  // `getBy...` methods will "throw" an error if the element is not found:
+  // const recipeForm = screen.getByText("Instructions:");
+
+  expect(recipeForm).toBeNull();
+  userEvent.click(screen.getByText("Add Recipe"));
+
+  expect(screen.getByLabelText("Instructions:")).toBeInTheDocument();
 });
+
+test('typing in the recipe name makes the recipe name appear in the input', async () => {
+  render(<App />);
+
+  const recipeName = 'No pockets';
+  userEvent.click(screen.getByText("Add Recipe"));
+  await userEvent.type(screen.getByLabelText('Recipe name:'), recipeName);
+
+  expect(screen.getByLabelText('Recipe name:').value).toEqual(recipeName);
+})
